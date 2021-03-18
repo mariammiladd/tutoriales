76c474bc7c4746b210a8efb56579132b54e915f6
Marian Milad Fernandez <mmiladfe@everis.com>
2021-03-18 12:05:38 +0100

    Terminado tutorial

--- src/main/java/com/init/product/config/SwaggerConfig.java
+++ src/main/java/com/init/product/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.unit.product.config;
+package com.init.product.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
